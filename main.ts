2193a05767f2469957a566b5ec0dd6e07d20c2db
Tom McAdam <tmcadam@users.noreply.github.com>
2023-10-23 12:50:21 +0900

    Basic robot code

--- main.ts
+++ main.ts
@@ -1,2 +1,22 @@
-radio.setGroup(1)
-led.plot(0, 0)
+let distance = 0
+basic.forever(function () {
+    let v = 0
+    distance = TPBot.sonarReturn(TPBot.SonarUnit.Centimeters)
+    if (v == 1 && distance > 20) {
+        TPBot.setTravelSpeed(TPBot.DriveDirection.Forward, 100)
+    } else if (v == 2) {
+        TPBot.setTravelSpeed(TPBot.DriveDirection.Backward, 100)
+    } else if (v == 3) {
+        TPBot.setTravelSpeed(TPBot.DriveDirection.Left, 30)
+    } else if (v == 4) {
+        TPBot.setTravelSpeed(TPBot.DriveDirection.Right, 30)
+    } else if (v == 5) {
+        TPBot.headlightRGB(randint(0, 255), randint(0, 255), randint(0, 255))
+    } else if (v == 6) {
+    	
+    } else if (false) {
+    	
+    } else {
+    	
+    }
+})
