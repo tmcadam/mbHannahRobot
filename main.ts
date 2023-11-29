8aff5d6c470a9b8985925382025fbda6b53d17ba
Tom McAdam <tmcadam@users.noreply.github.com>
2023-11-29 15:38:34 +0900

    Update to stop motors when sticks released

--- main.ts
+++ main.ts
@@ -4,23 +4,25 @@ radio.onReceivedNumber(function (receivedNumber) {
 let distance = 0
 let v = 0
 radio.setGroup(1)
-TPBot.headlightColor(0xff0000)
+let color = 0
 basic.showIcon(IconNames.Tortoise)
 basic.forever(function () {
     distance = TPBot.sonarReturn(TPBot.SonarUnit.Centimeters)
     if (v == 1 && distance > 20) {
-        TPBot.setTravelSpeed(TPBot.DriveDirection.Forward, 100)
+        TPBot.setTravelSpeed(TPBot.DriveDirection.Forward, 80)
     } else if (v == 2) {
-        TPBot.setTravelSpeed(TPBot.DriveDirection.Backward, 100)
+        TPBot.setTravelSpeed(TPBot.DriveDirection.Backward, 60)
     } else if (v == 3) {
-        TPBot.setTravelSpeed(TPBot.DriveDirection.Left, 30)
+        TPBot.setTravelSpeed(TPBot.DriveDirection.Left, 40)
     } else if (v == 4) {
-        TPBot.setTravelSpeed(TPBot.DriveDirection.Right, 30)
+        TPBot.setTravelSpeed(TPBot.DriveDirection.Right, 40)
     } else if (v == 5) {
         TPBot.headlightRGB(randint(0, 255), randint(0, 255), randint(0, 255))
     } else if (v == 6) {
         TPBot.setServo(TPBot.ServoTypeList.S360, TPBot.ServoList.S1, 180)
     } else if (v == 7) {
         TPBot.setServo(TPBot.ServoTypeList.S360, TPBot.ServoList.S1, 270)
+    } else {
+        TPBot.stopCar()
     }
 })
