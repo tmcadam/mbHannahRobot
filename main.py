d894f0d1790a3ceeaa5b0915c13da33be496d5a2
Tom McAdam <tmcadam@users.noreply.github.com>
2023-11-30 17:06:16 +0900

    Removed distance measurement due to servo being in front of sensor

--- main.py
+++ main.py
@@ -3,29 +3,62 @@ def on_received_number(receivedNumber):
     v = receivedNumber
 radio.on_received_number(on_received_number)
 
-distance = 0
-v = 0
-radio.set_group(1)
-TPBot.headlight_rgb(randint(0, 255), randint(0, 255), randint(0, 255))
-basic.show_icon(IconNames.TORTOISE)
-
-def on_forever():
-    global distance
-    distance = TPBot.sonar_return(TPBot.SonarUnit.CENTIMETERS)
+def manualControl():
     if v == 1 and distance > 20:
         TPBot.set_travel_speed(TPBot.DriveDirection.FORWARD, 100)
     elif v == 2:
-        TPBot.set_travel_speed(TPBot.DriveDirection.BACKWARD, 100)
+        TPBot.set_travel_speed(TPBot.DriveDirection.BACKWARD, 50)
     elif v == 3:
-        TPBot.set_travel_speed(TPBot.DriveDirection.LEFT, 30)
+        TPBot.set_travel_speed(TPBot.DriveDirection.LEFT, 40)
     elif v == 4:
-        TPBot.set_travel_speed(TPBot.DriveDirection.RIGHT, 30)
-    elif v == 5:
-        TPBot.headlight_rgb(randint(0, 255), randint(0, 255), randint(0, 255))
+        TPBot.set_travel_speed(TPBot.DriveDirection.RIGHT, 40)
     elif v == 6:
-        pass
-    elif False:
-        pass
+        TPBot.set_servo(TPBot.ServoTypeList.S360, TPBot.ServoList.S1, 270)
+    elif v == 7:
+        TPBot.set_servo(TPBot.ServoTypeList.S360, TPBot.ServoList.S1, 180)
     else:
-        pass
+        TPBot.stop_car()
+def lineFollow():
+    if TPBot.track_line(TPBot.TrackingState.L_R_LINE) and distance > 12:
+        TPBot.set_wheels(20, 20)
+    elif TPBot.track_line(TPBot.TrackingState.L_UNLINE_R_LINE):
+        TPBot.set_wheels(40, 10)
+    elif TPBot.track_line(TPBot.TrackingState.L_LINE_R_UNLINE):
+        TPBot.set_wheels(10, 40)
+    else:
+        TPBot.stop_car()
+distance = 0
+v = 0
+mode = 0
+radio.set_group(1)
+TPBot.headlight_color(0xff0000)
+basic.show_icon(IconNames.TORTOISE)
+
+def on_forever():
+    global distance, mode
+    distance = TPBot.sonar_return(TPBot.SonarUnit.CENTIMETERS)
+    if v == 9:
+        mode = 0
+    elif v == 10:
+        mode = 1
+    if mode == 0:
+        manualControl()
+    else:
+        lineFollow()
+    if input.acceleration(Dimension.X) < -250:
+        basic.show_leds("""
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            """)
+    elif input.acceleration(Dimension.X) > 250:
+        basic.show_leds("""
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            """)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -2,38 +2,36 @@ radio.onReceivedNumber(function (receivedNumber) {
     v = receivedNumber
 })
 function manualControl () {
-    if (v == 1 && distance > 20) {
+    if (v == 1) {
         TPBot.setTravelSpeed(TPBot.DriveDirection.Forward, 100)
     } else if (v == 2) {
-        TPBot.setTravelSpeed(TPBot.DriveDirection.Backward, 100)
+        TPBot.setTravelSpeed(TPBot.DriveDirection.Backward, 50)
     } else if (v == 3) {
-        TPBot.setTravelSpeed(TPBot.DriveDirection.Left, 30)
+        TPBot.setTravelSpeed(TPBot.DriveDirection.Left, 40)
     } else if (v == 4) {
-        TPBot.setTravelSpeed(TPBot.DriveDirection.Right, 30)
-    } else if (v == 5) {
-        TPBot.headlightRGB(randint(0, 255), randint(0, 255), randint(0, 255))
+        TPBot.setTravelSpeed(TPBot.DriveDirection.Right, 40)
     } else if (v == 6) {
-        TPBot.setServo(TPBot.ServoTypeList.S360, TPBot.ServoList.S1, 180)
-    } else if (v == 7) {
         TPBot.setServo(TPBot.ServoTypeList.S360, TPBot.ServoList.S1, 270)
+    } else if (v == 7) {
+        TPBot.setServo(TPBot.ServoTypeList.S360, TPBot.ServoList.S1, 180)
     } else {
         TPBot.stopCar()
     }
 }
 function lineFollow () {
-    if (TPBot.trackLine(TPBot.TrackingState.L_R_line) && distance > 20) {
+    if (TPBot.trackLine(TPBot.TrackingState.L_R_line)) {
         TPBot.setWheels(20, 20)
     } else if (TPBot.trackLine(TPBot.TrackingState.L_unline_R_line)) {
-        TPBot.setWheels(40, 0)
+        TPBot.setWheels(40, 10)
     } else if (TPBot.trackLine(TPBot.TrackingState.L_line_R_unline)) {
-        TPBot.setWheels(0, 40)
+        TPBot.setWheels(10, 40)
     } else {
         TPBot.stopCar()
     }
 }
+let mode = 0
 let distance = 0
 let v = 0
-let mode = 0
 radio.setGroup(1)
 TPBot.headlightColor(0xff0000)
 basic.showIcon(IconNames.Tortoise)
